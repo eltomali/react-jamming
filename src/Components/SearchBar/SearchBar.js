5b072c998e245205001d96b6655a05c832f3e8a8
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2019-10-13 12:08:20 +0200

    hook up search bar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,11 +2,26 @@ import React, {Component} from 'react';
 import './SearchBar.css'
 
 class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      query: ''
+    }
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+  search() {
+    this.props.search(this.state.query);
+  }
+
+  handleTermChange(evt) {
+    this.setState({query: evt.target.value});
+  }
   render() {
     return (
     <div className="SearchBar">
-      <input placeholder="Enter A Song, Album, or Artist" />
-      <button className="SearchButton">SEARCH</button>
+      <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
+      <button className="SearchButton" >SEARCH</button>
     </div>
     );
   }
